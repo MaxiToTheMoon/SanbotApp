f8f27ea7be1e1b0648e2f527d8359222e1ad3ca6
Massimiliano Bettarello <bettarello.massimiliano@educ.di.unito.it./gradlew>
2025-02-18 16:46:33 +0100

    commit

--- app/src/main/java/com/unito/sanbotapp/InteractActivity.java
+++ app/src/main/java/com/unito/sanbotapp/InteractActivity.java
@@ -1,5 +1,27 @@
 package com.unito.sanbotapp;
 
+import android.os.Bundle;
+
+import com.sanbot.opensdk.base.TopBaseActivity;
+import com.sanbot.opensdk.beans.FuncConstant;
+import com.sanbot.opensdk.function.unit.SpeechManager;
+
 //attività per chiedere se proseguire le spiegazioni, faq e eventuali aggiunte come l'uso di IA per l'interazione con l'utente
-public class InteractActivity {
+public class InteractActivity extends TopBaseActivity{
+    private final static String TAG = "INTERACT";
+
+    SpeechManager speechManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceSTate) {
+        register(InteractActivity.class);
+        super.onCreate(savedInstanceSTate);
+
+        speechManager = (SpeechManager) getUnitManager(FuncConstant.SPEECH_MANAGER);
+
+    }
+
+    @Override
+    protected void onMainServiceConnected() {
+    }
 }
